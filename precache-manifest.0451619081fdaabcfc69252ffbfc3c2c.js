9b715907de8870c401d79b25ff4e4776f67307c8
Anton Sokolov <anton291291@gmail.com>
2019-09-08 00:23:00 +0300

    Updates

---
+++ precache-manifest.0451619081fdaabcfc69252ffbfc3c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1d55a12745ce68ecc974a369851d9b",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "5ae135909aff56d40697",
+    "url": "/blog/static/css/main.671d074e.chunk.css"
+  },
+  {
+    "revision": "bdfca3e7ffee074443a3",
+    "url": "/blog/static/js/2.5440e9ee.chunk.js"
+  },
+  {
+    "revision": "5ae135909aff56d40697",
+    "url": "/blog/static/js/main.e2412fd0.chunk.js"
+  },
+  {
+    "revision": "eeecc51c3b4e3b0a67a2",
+    "url": "/blog/static/js/runtime~main.516a48d0.js"
+  }
+]);
